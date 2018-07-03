6b72efd6bf5929efdbf3388520399021e6e76d96
David Barocio <baropsx2@gmail.com>
2018-07-03 14:21:18 -0500

    Arrays final

--- SeleniumCourse/src/JavaSelenium/ArraysDemo.java
+++ SeleniumCourse/src/JavaSelenium/ArraysDemo.java
@@ -1,24 +1,48 @@
 package JavaSelenium;
 
+import java.util.*;
+
 public class ArraysDemo {
 	public static void main(String[] args) {
+					
+		//Imprime Arrays con enteros en varias líneas (Int)
+		
+		/*
 		int[] myIntArray1;
 		myIntArray1 = new int[10];
-		
 		myIntArray1[0] = 100;
 		myIntArray1[1] = 90;
+		*/
 		
-		//Arrays con enteros (Int)
+		
+		int[] myIntArray1 = {100, 90, 80};
 		System.out.println("0 index: " + myIntArray1[0]);
 		System.out.println("1st index: " + myIntArray1[1]);
 		System.out.println("2nd index: "+ myIntArray1[2]);
 		
-		//Arrays con texto (String)
+		//Imprime Arrays con texto en una sola línea (String)
 		String[] myStringArray1 = {"bmw", "audi", "honda"};
 		System.out.println("0 index: " + myStringArray1[0]);
 		System.out.println("1st index: " + myStringArray1[1]);
 		System.out.println("2nd index: "+ myStringArray1[2]);
 		
+		int len1 = myIntArray1.length;
+		int len2 = myStringArray1.length;
+		
+		System.out.println("Int array lenght: " + len1);
+		System.out.println("String array lenght: " + len2);
+		
+		for (int i = 0; i < len1; i++){
+			System.out.println(myIntArray1[i]);
+		}
+		
+		Arrays.sort(myIntArray1);
+		
+		for (int i = 0; i < len1; i++){
+			System.out.println(myIntArray1[i]);
+		}
+
+		//System.arraycopy(src, srcPos, dest, destPos, length);
 	}
 
 }
