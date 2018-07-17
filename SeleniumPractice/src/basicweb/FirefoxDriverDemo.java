7431ed914134947739dab474c251139e5dc7bd61
David Barocio <baropsx2@gmail.com>
2018-07-17 11:56:58 -0500

    New commit

---
+++ SeleniumPractice/src/basicweb/FirefoxDriverDemo.java
@@ -0,0 +1,18 @@
+package basicweb;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxDriverDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:/Users/BoXX/eclipse-workspace/Selenium/geckodriver.exe");
+		driver = new FirefoxDriver();
+		String baseURL = "http://www.facebook.com";
+		
+		driver.get(baseURL);
+
+	}
+
+}
