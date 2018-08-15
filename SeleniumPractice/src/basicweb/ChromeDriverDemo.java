8b669b55cdbf8bdb5da9cfa61ca8f762cccda89b
baropsx2 <baropsx2@gmail.com>
2018-08-14 22:29:32 -0500

    Selenium config

--- SeleniumPractice/src/basicweb/ChromeDriverDemo.java
+++ SeleniumPractice/src/basicweb/ChromeDriverDemo.java
@@ -13,15 +13,16 @@ public class ChromeDriverDemo {
 		String baseURL = "http://www.google.com";
 		WebDriver driver;
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\BoXX\\eclipse-workspace\\Selenium\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(baseURL);
-		driver.findElement(By.id("lst-ib")).sendKeys("emulatronia");
+		driver.findElement(By.id("lst-ib")).sendKeys("emulatronia\n");
 	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.findElement(By.id("btnK"));
-	 // driver.findElement(By.xpath("//*[@id=\"u_0_2\"]")).click();
-		
+		driver.findElement(By.xpath("//button[@name='btnG']")).click();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.findElement(By.xpath("(//h3[@class='r']/a)[3]")).click();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
 	}
 
 }
