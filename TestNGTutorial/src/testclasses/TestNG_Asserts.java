c5bfe1c23070f533df1d3d761b7c31ec91b2d9f0
baropsx2 <baropsx2@gmail.com>
2018-08-21 19:05:45 -0500

    1

--- TestNGTutorial/src/testclasses/TestNG_Asserts.java
+++ TestNGTutorial/src/testclasses/TestNG_Asserts.java
@@ -1,5 +1,6 @@
 package testclasses;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import appcode.SomeClassToTest;
 
@@ -7,23 +8,29 @@ public class TestNG_Asserts {
 	
 	@Test
 	public void testSum() {
-		System.out.println("Running Test -> testSum");
+		System.out.println("\nRunning Test -> testSum");
 		SomeClassToTest obj = new SomeClassToTest();
 		int result = obj.sumNumbers(1, 2);
+		Assert.assertEquals(result, 3);
 	}
 	
 	@Test
 	public void testStrings() {
-		System.out.println("Running Test -> testStrings");
+		System.out.println("\nRunning Test -> testStrings");
 		String expectedString = "Hello World";
 		SomeClassToTest obj = new SomeClassToTest();
+		String result = obj.addStrings("Hello", "World");
+		Assert.assertEquals(result, expectedString);
 	}
 	
 	@Test
 	public void testArrays() {
-		System.out.println("Running Test -> testArrays");
+		System.out.println("\nRunning Test -> testArrays\n");
 		int[] expectedArray = {1, 2, 3};
 		SomeClassToTest obj = new SomeClassToTest();
+		int[] result = obj.getArray();
+		Assert.assertEquals(result, expectedArray);
+		System.out.println("\nEnd Test -> testArrays");
 	}
 
 }
